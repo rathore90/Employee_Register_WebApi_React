da580030c6f37c653c6e5643914a9339fd28ba81
Pardeep Rathore <39980537+rathore90@users.noreply.github.com>
2021-01-09 22:51:13 -0800

    initial commit

---
+++ Employee_Record/Employee_Record_WebApi/Models/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Record_WebApi.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string EmployeeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Occupation { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+    }
+}
